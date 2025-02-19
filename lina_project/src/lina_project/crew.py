f3aafeb212fa1102d17a035cdad5496c56d77004
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2025-02-19 11:11:02 +0100

    Removd latest dev

--- lina_project/src/lina_project/crew.py
+++ lina_project/src/lina_project/crew.py
@@ -59,7 +59,7 @@ class LinaProject():
 	def reporting_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			output_file='output/report.md' # This is the file that will be contain the final report.
 		)
 
 	@crew
